7eb1fe5170d05a16a77ea4f61cd1d1dd9f0116dd
candac <cuau02@gmail.com>
2025-05-20 21:08:09 +0800

    fixes

--- reward_agents/__init__.py
+++ reward_agents/__init__.py
@@ -1,1 +1,1 @@
-self.model_name_or_path = model_path
+
--- reward_agents/agent_base.py
+++ reward_agents/agent_base.py
@@ -6,7 +6,10 @@ class AgentBase:
     def __init__(self, agent_config):
         self.agent_config = agent_config
         self.name = self.__class__.__name__.replace("Agent", "")  # e.g. "FactualityAgent" -> "Factuality"
+        
         model_path = os.path.join(agent_config["model_path"], agent_config["model_name"])
+        self.model_name_or_path = model_path
+
         dtype = getattr(torch, agent_config.get("dtype", "float16"))
         self.tokenizer = AutoTokenizer.from_pretrained(model_path, use_fast=True)
         self.model = AutoModelForCausalLM.from_pretrained(
